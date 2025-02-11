037e5357a1088653d2397b7559c38b9319927212
irukahaoruka <erika03301109@gmail.com>
2025-02-11 22:00:01 +0900

    first commit

---
+++ WithSwiftUI/WithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WithSwiftUIApp.swift
+//  WithSwiftUI
+//
+//  Created by 櫻井絵理香 on 2025/02/11.
+//
+
+import SwiftUI
+
+@main
+struct WithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
